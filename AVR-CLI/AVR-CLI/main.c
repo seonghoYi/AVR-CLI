9142c00166575bbabe350d6855a030271e6f0edd
seonghoYi <sh.Lee4781@gmail.com>
2021-02-22 17:06:18 +0900

    Add AVR-CLI project

---
+++ AVR-CLI/AVR-CLI/main.c
@@ -0,0 +1,18 @@
+/*
+ * AVR-CLI.c
+ *
+ * Created: 2021-02-22 오후 5:04:38
+ * Author : LSH
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
