fe39a20b4871c66380a2e94f1eb856171ecc75dd
seonghoYi <sh.Lee4781@gmail.com>
2021-02-25 15:30:37 +0900

    Modified [atmega128_DRV_uart.c]

--- AVR-CLI/AVR-CLI/src/lib/Drivers/Src/atmega128_DRV_uart.c
+++ AVR-CLI/AVR-CLI/src/lib/Drivers/Src/atmega128_DRV_uart.c
@@ -7,11 +7,26 @@ USART_TypeDef USART1_descripter = {&UDR1, &UCSR1A, &UCSR1B, &UCSR1C, &UBRR1H, &U
 
 
 
-
-StatusTypeDef UART0_Init(UART_HandleTypeDef *huart)
+StatusTypeDef UART_Init(UART_HandleTypeDef *huart)
 {
 	uint32_t temp;
+	USART_TypeDef *usart = NULL;
 	if (huart == NULL)
+	{
+		return ERROR;
+	}
+	
+	switch(huart->USARTn)
+	{
+		case USART0:
+		usart = &USART0_descripter;
+		break;
+		case USART1:
+		usart = &USART1_descripter;
+		break;
+	}
+	
+	if (usart == NULL)
 	{
 		return ERROR;
 	}
@@ -20,15 +35,15 @@ StatusTypeDef UART0_Init(UART_HandleTypeDef *huart)
 	{
 		case UART_OVERSAMPLING_8:
 		temp = (float)(F_CPU / (float)(16 * huart->Init.BaudRate)) - 0.5; //-1.0 + 0.5 => for rounding
-		UBRR0H = (temp >> 8) & 0xFF;
-		UBRR0L = temp & 0xFF;
-		CLRB(UCSR0A, 1);
+		*(usart->UBRRnH) = (temp >> 8) & 0xFF;
+		*(usart->UBRRnL) = temp & 0xFF;
+		CLRB(*(usart->UCSRnA), 1);
 		break;
 		case UART_OVERSAMPLING_16:
 		temp = (float)(F_CPU / (float)(8 * huart->Init.BaudRate)) - 0.5;
-		UBRR0H = (temp >> 8) & 0xFF;
-		UBRR0L = temp & 0xFF;
-		SETB(UCSR0A, 1);
+		*(usart->UBRRnH) = (temp >> 8) & 0xFF;
+		*(usart->UBRRnL) = temp & 0xFF;
+		SETB(*(usart->UCSRnA), 1);
 		break;
 		default:
 		break;
@@ -37,29 +52,29 @@ StatusTypeDef UART0_Init(UART_HandleTypeDef *huart)
 	switch(huart->Init.WordLength)
 	{
 		case UART_WORDLENGTH_8B:
-		CLRB(UCSR0B, 2);
-		SETB(UCSR0C, 2);
-		SETB(UCSR0C, 1);
+		CLRB(*(usart->UCSRnB), 2);
+		SETB(*(usart->UCSRnC), 2);
+		SETB(*(usart->UCSRnC), 1);
 		break;
 		case UART_WORDLENGTH_5B:
-		CLRB(UCSR0B, 2);
-		CLRB(UCSR0C, 2);
-		CLRB(UCSR0C, 1);
+		CLRB(*(usart->UCSRnB), 2);
+		CLRB(*(usart->UCSRnC), 2);
+		CLRB(*(usart->UCSRnC), 1);
 		break;
 		case UART_WORDLENGTH_6B:
-		CLRB(UCSR0B, 2);
-		CLRB(UCSR0C, 2);
-		SETB(UCSR0C, 1);
+		CLRB(*(usart->UCSRnB), 2);
+		CLRB(*(usart->UCSRnC), 2);
+		SETB(*(usart->UCSRnC), 1);
 		break;
 		case UART_WORDLENGTH_7B:
-		CLRB(UCSR0B, 2);
-		SETB(UCSR0C, 2);
-		CLRB(UCSR0C, 1);
+		CLRB(*(usart->UCSRnB), 2);
+		SETB(*(usart->UCSRnC), 2);
+		CLRB(*(usart->UCSRnC), 1);
 		break;
 		case UART_WORDLENGTH_9B:
-		SETB(UCSR0B, 2);
-		SETB(UCSR0C, 2);
-		SETB(UCSR0C, 1);
+		SETB(*(usart->UCSRnB), 2);
+		SETB(*(usart->UCSRnC), 2);
+		SETB(*(usart->UCSRnC), 1);
 		break;
 		default:
 		break;
@@ -68,26 +83,26 @@ StatusTypeDef UART0_Init(UART_HandleTypeDef *huart)
 	switch(huart->Init.StopBits)
 	{
 		case UART_STOPBITS_1:
-		CLRB(UCSR0C, 4);
+		CLRB(*(usart->UCSRnC), 4);
 		break;
 		case UART_STOPBITS_2:
-		SETB(UCSR0C, 4);
+		SETB(*(usart->UCSRnC), 4);
 		break;
 	}
 	
 	switch(huart->Init.Parity)
 	{
 		case UART_PARITY_NONE:
-		CLRB(UCSR0C, 6);
-		CLRB(UCSR0C, 5);
+		CLRB(*(usart->UCSRnC), 6);
+		CLRB(*(usart->UCSRnC), 5);
 		break;
 		case UART_PARITY_EVEN:
-		SETB(UCSR0C, 6);
-		CLRB(UCSR0C, 5);
+		SETB(*(usart->UCSRnC), 6);
+		CLRB(*(usart->UCSRnC), 5);
 		break;
 		case  UART_PARITY_ODD:
-		SETB(UCSR0C, 6);
-		SETB(UCSR0C, 5);
+		SETB(*(usart->UCSRnC), 6);
+		SETB(*(usart->UCSRnC), 5);
 		break;
 		default:
 		break;
@@ -97,143 +112,20 @@ StatusTypeDef UART0_Init(UART_HandleTypeDef *huart)
 	switch(huart->Init.Mode)
 	{
 		case UART_MODE_RX:
-		SETB(UCSR0B, 4);
+		SETB(*(usart->UCSRnB), 4);
 		break;
 		case UART_MODE_TX:
-		SETB(UCSR0B, 3);
+		SETB(*(usart->UCSRnB), 3);
 		break;
 		case UART_MODE_RX_TX:
-		SETB(UCSR0B, 3);
-		SETB(UCSR0B, 4);
+		SETB(*(usart->UCSRnB), 3);
+		SETB(*(usart->UCSRnB), 4);
 		break;
 		default:
 		break;
 	}
 	
 	
-	return OK;
-}
-
-StatusTypeDef UART1_Init(UART_HandleTypeDef *huart)
-{
-	uint32_t temp;
-	if (huart == NULL)
-	{
-		return ERROR;
-	}
-	
-	switch(huart->Init.OverSampling)
-	{
-		case UART_OVERSAMPLING_8:
-		temp = (F_CPU / (16 * huart->Init.BaudRate)) - 1;
-		UBRR1H = (temp >> 8) & 0xFF;
-		UBRR1L = temp & 0xFF;
-		CLRB(UCSR1A, 1);
-		break;
-		case UART_OVERSAMPLING_16:
-		temp = (F_CPU / (8 * huart->Init.BaudRate)) - 1;
-		UBRR1H = (temp >> 8) & 0xFF;
-		UBRR1L = temp & 0xFF;
-		SETB(UCSR1A, 1);
-		break;
-		default:
-		break;
-	}
-	
-	switch(huart->Init.WordLength)
-	{
-		case UART_WORDLENGTH_8B:
-		CLRB(UCSR1B, 2);
-		SETB(UCSR1C, 2);
-		SETB(UCSR1C, 1);
-		break;
-		case UART_WORDLENGTH_5B:
-		CLRB(UCSR1B, 2);
-		CLRB(UCSR1C, 2);
-		CLRB(UCSR1C, 1);
-		break;
-		case UART_WORDLENGTH_6B:
-		CLRB(UCSR1B, 2);
-		CLRB(UCSR1C, 2);
-		SETB(UCSR1C, 1);
-		break;
-		case UART_WORDLENGTH_7B:
-		CLRB(UCSR1B, 2);
-		SETB(UCSR1C, 2);
-		CLRB(UCSR1C, 1);
-		break;
-		case UART_WORDLENGTH_9B:
-		SETB(UCSR1B, 2);
-		SETB(UCSR1C, 2);
-		SETB(UCSR1C, 1);
-		break;
-		default:
-		break;
-	}
-	
-	switch(huart->Init.StopBits)
-	{
-		case UART_STOPBITS_1:
-		CLRB(UCSR1C, 4);
-		break;
-		case UART_STOPBITS_2:
-		SETB(UCSR1C, 4);
-		break;
-	}
-	
-	switch(huart->Init.Parity)
-	{
-		case UART_PARITY_NONE:
-		CLRB(UCSR1C, 6);
-		CLRB(UCSR1C, 5);
-		break;
-		case UART_PARITY_EVEN:
-		SETB(UCSR1C, 6);
-		CLRB(UCSR1C, 5);
-		break;
-		case  UART_PARITY_ODD:
-		SETB(UCSR1C, 6);
-		SETB(UCSR1C, 5);
-		break;
-		default:
-		break;
-	}
-	
-	switch(huart->Init.Mode)
-	{
-		case UART_MODE_RX:
-		SETB(UCSR1B, 4);
-		break;
-		case UART_MODE_TX:
-		SETB(UCSR1B, 3);
-		break;
-		case UART_MODE_RX_TX:
-		SETB(UCSR1B, 3);
-		SETB(UCSR1B, 4);
-		break;
-		default:
-		break;
-	}
-	
-	
-	return OK;
-}
-
-
-
-StatusTypeDef UART_Init(UART_HandleTypeDef *huart)
-{
-	switch(huart->USARTn)
-	{
-		case USART0:
-			UART0_Init(huart);
-		break;
-		case USART1:
-			UART1_Init(huart);
-		break;
-		default:
-		break;
-	}
 	return OK;
 }
 
@@ -241,12 +133,29 @@ StatusTypeDef UART_Transmit(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t 
 {
 	uint8_t		*pdata8bits = NULL;
 	uint16_t	*pdata16bits = NULL;
-
+	USART_TypeDef *usart = NULL;
 	
 	huart->ErrorCode = UART_ERROR_NONE;
 	
 	huart->TxXferSize = Size;
 	huart->TxXferCount = Size;
+	
+	switch(huart->USARTn)
+	{
+		case USART0:
+		usart = &USART0_descripter;
+		break;
+		case USART1:
+		usart = &USART1_descripter;
+		break;
+		default:
+		break;
+	}
+	
+	if (huart == NULL)
+	{
+		return ERROR;
+	}
 	
 	if (huart->Init.WordLength == UART_WORDLENGTH_9B && huart->Init.Parity == UART_PARITY_NONE)
 	{
@@ -259,40 +168,18 @@ StatusTypeDef UART_Transmit(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t 
 		pdata16bits = NULL;
 	}
 	
-	switch(huart->USARTn)
-	{
-		case USART0:
-		for(int i = 0; i < huart->TxXferCount; i++)
-		{
-			while (!(UCSR0A & 0x20));
-			if (pdata16bits == NULL)
-			{
-				UDR0 = *(pdata8bits + i);
-			}
-			else
-			{
-				UDR0 = *(pdata16bits + i);
-			}
-			
-		}
-		break;
-		case USART1:
-		for(int i = 0; i < huart->TxXferCount; i++)
-		{
-			while (!(UCSR1A & 0x20));
-			if (pdata16bits == NULL)
-			{
-				UDR1 = *(pdata8bits + i);
-			}
-			else
-			{
-				UDR1 = *(pdata16bits + i);
-			}
 
+	for(int i = 0; i < huart->TxXferCount; i++)
+	{
+		while (!(*(usart->UCSRnA) & 0x20));
+		if (pdata16bits == NULL)
+		{
+			*(usart->UDRn) = *(pdata8bits + i);
 		}
-		break;
-		default:
-		break;
+		else
+		{
+			*(usart->UDRn) = *(pdata16bits + i);
+		}
 	}
 	
 	
@@ -303,12 +190,30 @@ StatusTypeDef UART_Receive(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t S
 {
 	uint8_t		*pdata8bits = NULL;
 	uint16_t	*pdata16bits = NULL;
-
+	USART_TypeDef *usart = NULL;
 	
 	huart->ErrorCode = UART_ERROR_NONE;
 	
 	huart->RxXferSize = Size;
 	huart->RxXferCount = Size;
+	
+	
+	switch(huart->USARTn)
+	{
+		case USART0:
+		usart = &USART0_descripter;
+		break;
+		case USART1:
+		usart = &USART1_descripter;
+		break;
+		default:
+		break;
+	}
+	
+	if (huart == NULL)
+	{
+		return ERROR;
+	}
 	
 	if (huart->Init.WordLength == UART_WORDLENGTH_9B && huart->Init.Parity == UART_PARITY_NONE)
 	{
@@ -320,43 +225,20 @@ StatusTypeDef UART_Receive(UART_HandleTypeDef *huart, uint8_t *pData, uint16_t S
 		pdata8bits = pData;
 		pdata16bits = NULL;
 	}
-	
-	switch(huart->USARTn)
+
+	for(int i = 0; i < huart->TxXferCount; i++)
 	{
-		case USART0:
-		for(int i = 0; i < huart->TxXferCount; i++)
+		while (!(*(usart->UCSRnA) & 0x80))
 		{
-			while (!(UCSR0A & 0x80))
+			if (pdata16bits == NULL)
 			{
-				if (pdata16bits == NULL)
-				{
-					*(pdata8bits + i) = UDR0;
-				}
-				else
-				{
-					*(pdata16bits + i) = UDR0;
-				}
+				*(pdata8bits + i) = *(usart->UDRn);
+			}
+			else
+			{
+				*(pdata16bits + i) = *(usart->UDRn);
 			}
 		}
-		break;
-		case USART1:
-		for(int i = 0; i < huart->TxXferCount; i++)
-		{
-			while (!(UCSR1A & 0x80))
-			{
-				if (pdata16bits == NULL)
-				{
-					*(pdata8bits + i) = UDR1;
-				}
-				else
-				{
-					*(pdata16bits + i) = UDR1;
-				}
-			}
-		}
-		break;
-		default:
-		break;
 	}
 
 	return OK;
